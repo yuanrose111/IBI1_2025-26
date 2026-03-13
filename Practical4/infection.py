3c2230a155b44b3f5188d443cd0584b0a20207e5
yuanrose111 <yuan1.25@intl.zju.edu.cn>
2026-03-13 10:22:11 +0800

    Update infection.py

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -1,10 +1,8 @@
-
 # 1. Define basic variables：初始感染人数initial_infected、日增长率growth_rate（小数）、总学生数total_students=91
 # 2. Define a counter：days=0（records the number of infected days），current_infected=initial_infected（current number of infected people, initial value
 # 3. Create a while loop：continuously execute the loop when current_student <total_student
 # 4. loop operation：print the number of infected people onn the current day --calculate the number of infected people on the next day --add 1 to the number of days
 # 5. After the cycle ends : print the final number of infected individuals(≥91) and the total number of infected days
-
 
 # define basic variables（example)
 initial_infected = 5
@@ -15,14 +13,12 @@ total_students = 91
 current_infected = initial_infected  
 days = 0 #initial is 0
 print("daily changes in IBI1 class infected count：")
-print("-" * 40)
 
 # while：calculate when infected don not finish
 while current_infected < total_students:
     print(f"at {days} day：{current_infected:.1f} people infected")  
     current_infected = current_infected * (1 + growth_rate)  #calculate the number of next day
     days += 1  
-
 
 print(f"at {days} day：{current_infected:.1f} people infected （all people (91) in the class have been infected)")
 print("-" * 40)
